3fa68fb7f3a3ad448d91371860d4cba31435f0fa
telugu srikanth <sri.tech81@gmail.com>
2023-09-15 18:03:42 +0530

    clientsri

---
+++ sriClient/src/test/java/com/example/sriClient/SriClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sriClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
